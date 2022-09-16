c4a196ede95d376787fc88fea48d4761c2761b81
Guillaume <guillaume.militello@gmail.com>
2022-09-16 12:47:12 -0400

    Extend logging with callback

--- core/logging.hpp
+++ core/logging.hpp
@@ -1,9 +1,55 @@
+#pragma once
+
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <sstream>
+#include <string>
+
 #include "core/libcamera_app.hpp"
 
-#define LOG(level, text)                                                                                               \
-	do                                                                                                                 \
-	{                                                                                                                  \
-		if (LibcameraApp::GetVerbosity() >= level)                                                                     \
-			std::cerr << text << std::endl;                                                                            \
-	} while (0)
-#define LOG_ERROR(text) std::cerr << text << std::endl
+struct Logging
+{
+	static inline void logSetCallback(std::function<void(unsigned, const std::string&)> callback)
+	{
+		callback_ = callback;
+	}
+
+	static inline void log(unsigned level, std::string&& text)
+	{
+		if (callback_)
+		{
+			callback_(level, text);
+		}
+		else
+		{
+			std::cerr << text << std::endl;
+		}
+	}
+
+private:
+	inline static std::function<void(unsigned, const std::string&)> callback_ = nullptr;
+};
+
+
+#define LOG(level, text) 								\
+		do										 		\
+		{												\
+			if (LibcameraApp::GetVerbosity() >= level)	\
+			{											\
+				std::ostringstream ss;					\
+				ss << (text);							\
+				Logging::log(level, ss.str()); 			\
+			}											\
+		}										 		\
+		while(0)
+
+
+#define LOG_ERROR(text) 	 					 \
+		do										 \
+		{										 \
+			std::ostringstream ss;				 \
+			ss << (text);						 \
+			Logging::log(unsigned(0), ss.str()); \
+		}										 \
+		while(0)
